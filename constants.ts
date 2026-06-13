55ad6bbb553d5070723438f9a45a1bb99955dfc9
mystarz260116 <ricckikaku@gmail.com>
2026-06-13 12:45:04 +0900

    3次元有床プリント義歯の価格を「おたずねください」にした

--- constants.ts
+++ constants.ts
@@ -87,7 +87,7 @@ export const INITIAL_PRICE_DATA: PriceListData = {
         { name: '線造双子鉤', price: '2,000' },
         { name: '線造単鉤', price: '1,100' },
         { name: 'コンビネーション鉤', price: '2,200' },
-        { name: '３次元有床プリント義歯', price: '28,000' },
+        { name: '３次元有床プリント義歯', price: 'お尋ねください' },
       ]
     },
     {
